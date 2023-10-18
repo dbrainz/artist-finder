326942459b4ad39e5731f54624273f98c081336c
Darren Brain <darren.brain@gmail.com>
2023-10-17 21:22:52 -0400

    Convert youtube api function to provide an array of objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ $(function() {
     })
 */
 
-
+getYTArtist("the fugees")
 
 
 });
--- assets/js/youtubeapi.js
+++ assets/js/youtubeapi.js
@@ -1,27 +1,54 @@
-// call the youtube api with the function getYTArtist(artist_name)
-// example : getYTArtist("metallica");
-// currently returns ten results - we can change that number to be able to be changed on the fly, or just adjust the hard-coded number in the query if needed
+// call the youtube api with the function getYTArtist(artist_name, num_of_results (optional))
+// example : getYTArtist("metallica") or getYTArtist("fugees", 20)
+// default number of results returned is 10
+//
+// returns an array of objects
+// name - title of the video
+// description - description of the video
+// url - url of the video
+// channelTitle - name of the youtube channel the video is attached to
+// channelURL - url of the video's channel
+// imageSm - url of the small thumbnail for the video - 120px x 90px
+// imageMd - url of the medium thumbnail for the video - 320px x 180px
+// imageLg - url of the large thumbnail for the video - 480px x 360px
 
 const YT_API_KEY="AIzaSyDvGaotukI76kg-Q_EaojpRgRGFatg0M7c";
 
+function getYTArtist(artistName, numOfResults=10) {
+    var ytArtistQueryStr = ""
+    var results=[];
 
-function getYTArtist(artistName) {
-    var ytArtistQueryStr = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=" + artistName + "&type=video&key=" + YT_API_KEY;
+    // remove single and double quotes, ampersands, and replace spaces with underscores
+    queryArtistName = artistName.replace(" & ", "_and_")
+        .replace("&", "_and_")
+        .replace(" ", "_")       
+        .replace("'","")
+        .replace('"',"");
+
+    console.log(queryArtistName);
+    ytArtistQueryStr = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=" + numOfResults + "&q=" + artistName + "&type=video&key=" + YT_API_KEY;
 
     fetch(ytArtistQueryStr)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
-                // test code that dumps the results to the page
-
+            // build an object for each video to add to the results array
             for (let x=0; x<data.items.length; x++) {
-                let videoID = data.items[x].id.videoId
-                let videoName = data.items[x].snippet.title
-                let videoImage = "<img src='" + data.items[x].snippet.thumbnails.default.url + "'>"
-                $("#yt-videos").append("<li><a href='https://www.youtube.com/watch?v=" + videoID + "' target='_blank'>" + videoImage + "  " + videoName +  "</a></li>")
+                let videoObj = {
+                    name : data.items[x].snippet.title,
+                    description : data.items[x].snippet.description,
+                    imageSm : data.items[x].snippet.thumbnails.default.url,
+                    imageMd : data.items[x].snippet.thumbnails.medium.url,
+                    imageLg : data.items[x].snippet.thumbnails.high.url,
+                    url : "https://www.youtube.com/watch?v=" + data.items[x].id.videoId,
+                    channelTitle : data.items[x].snippet.channelTitle,
+                    channelURL : "https://www.youtube.com/channel/" + data.items[x].snippet.channelID               
+                }
+                results.push(videoObj);
             }
+            console.log(results);
+            return results;
         });
 
 
