9d9572dc88efb3f01347da52c329df48487c5c19
Darren Brain <darren.brain@gmail.com>
2023-10-23 16:01:50 -0400

    Convert search list into links

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,9 +32,36 @@ function addPrevious(searchStr) {
 function displayPrevious(){
 
     var previousEl = $("#previousSearch")
-    previousEl.html('Previous Searches: ' + storedArtists.join(', ')); 
+    buildStr = "Previous Searches : "
+    //previousEl.html('Previous Searches: ' + storedArtists.join(', ')); 
+    previousEl.empty()
+    previousEl.append("Previous Searches : ")
+    for (x=0; x<storedArtists.length;x++) {
+        previousEl.append("<a href = '#' id='#prevSearch" + x + "' onclick=artistSearch('" + storedArtists[x] + "')>" + storedArtists[x] + "</a>")
+        if (x!=storedArtists.length-1) {
+            previousEl.append(", ")
+        }
+        listenEl = $("#prevSearch" + x)
+        listenEl.click(function() {
+            console.log("foo")
+            artistSearch(storedArtists[x])
+        })
+
+    }
+
 }
 
+function artistSearch(searchName) {
+
+    addPrevious(searchName);
+    var cleanName = formatUserInput(searchName);
+    var eventList = document.getElementById('event-list');
+    eventList.innerHTML = '';
+    getEvents(cleanName);
+    getYTArtist(searchName)
+    eventList.classList.add('myStyle');
+
+}
 $(function() {
 
     // load and display previous artist search list
@@ -46,9 +73,11 @@ $(function() {
     
         var artistNameInput = document.getElementById('artist-name');
         var artistName = artistNameInput.value;
+
+        artistSearch(artistName)
         
         // add new search to the previous searches list
-        addPrevious(artistName);
+        /*addPrevious(artistName);
     
         artistName = formatUserInput(artistName);
         var eventList = document.getElementById('event-list');
@@ -56,7 +85,7 @@ $(function() {
         getEvents(artistName);
         getYTArtist(artistNameInput.value)
         eventList.classList.add('myStyle');
-
+        */
     });
 
 });
